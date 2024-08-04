f4b3e356befcb72646506aaba104d690609fd3eb
junho <yunjunhojj@gmail.com>
2024-08-04 15:35:17 +0900

    ID param을 이용해서 하나의 Post 가져오기

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
 interface Post {
+  id: number;
   author: string;
   title: string;
   content: string;
@@ -9,18 +10,59 @@ interface Post {
   comments: number;
 }
 
+const posts: Post[] = [
+  {
+    id: 1,
+    author: 'John',
+    title: 'My first post',
+    content: 'Hello World!',
+    likes: 10,
+    comments: 5,
+  },
+  {
+    id: 2,
+    author: 'Jane',
+    title: 'My second post',
+    content: 'Hello World!',
+    likes: 5,
+    comments: 2,
+  },
+  {
+    id: 3,
+    author: 'John',
+    title: 'My third post',
+    content: 'Hello World!',
+    likes: 8,
+    comments: 3,
+  },
+];
+
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  // GET /posts
+  // Get all posts
+
+  // GET /posts/:id
+  // Get a post by id
+
+  // POST /posts
+  // Create a new post
+
+  // PUT /posts/:id
+  // Update a post by id
+
+  // DELETE /posts/:id
+  // Delete a post by id
+
   @Get()
-  getPost(): Post {
-    return {
-      author: 'John',
-      title: 'My first post',
-      content: 'Hello World!',
-      likes: 10,
-      comments: 5,
-    };
+  getPosts(): Post[] {
+    return posts;
+  }
+
+  @Get(':id')
+  getPost(@Param('id') id: number): Post {
+    return posts.find((post) => post.id === id);
   }
 }
